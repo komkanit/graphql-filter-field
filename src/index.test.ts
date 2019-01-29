3de2f394ea75daf9e0d976a24a87d7e94c5d610b
komcal <kom.sujautra@gmail.com>
2019-01-29 11:08:31 +0700

    setup test coverage

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,25 +1,42 @@
 import test from 'ava'
 import { GraphQLObjectType } from 'graphql'
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
-import { filterFields } from './'
+import { makeExecutableSchema, addMockFunctionsToSchema, transformSchema } from 'graphql-tools'
+import { filterFields, withFilterFields } from './'
+
+const typeDefs = `
+  type User {
+    id: ID
+    fullName: String
+    hello_delete: String
+  }
+
+  type Query {
+    users: [User]
+  }
+`
 
 test('filter field name end with _delete', (t) => {
-  const typeDefs = `
-    type User {
-      id: ID
-      fullName: String
-      hello_delete: String
-    }
-
-    type Query {
-      users: [User]
-    }
-  `
   const schema = makeExecutableSchema({ typeDefs })
   addMockFunctionsToSchema({ schema })
   const newSchema = filterFields(schema, (field) => {
     return field.name.endsWith('_delete')
   })
+  const userType = newSchema.getType('User') as GraphQLObjectType<any, any>
+  const fields = userType.getFields()
+  
+  t.is(fields['hello_delete'], undefined)
+  t.not(fields['id'], undefined)
+  t.not(fields['fullName'], undefined)
+})
+
+test('filter field using transformSchema', (t) => {
+  const schema = makeExecutableSchema({ typeDefs })
+  addMockFunctionsToSchema({ schema })
+  const newSchema = transformSchema(schema, [
+    withFilterFields((field) => {
+      return field.name.endsWith('_delete')
+    })
+  ])
   const userType = newSchema.getType('User') as GraphQLObjectType<any, any>
   const fields = userType.getFields()
   
